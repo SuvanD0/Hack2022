46369b4bf02cb367bb8155e72eb1db5c8db45cc7
Aaron-Dhillon <110361766+Aaron-Dhillon@users.noreply.github.com>
2022-10-08 18:29:57 -0400

    Update

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+
 data = st.container()
 
 max = st.container()
@@ -8,13 +9,10 @@ max = st.container()
 
 
 df = pd.read_csv('WTG A.csv')
+df[df['WTG-A (2.82-FDR 2A) - POWER'].str.contains('Bad')==False]
+
+
 with data:
     st.write(df.columns)
-
-
-with max:
-    
-    filtered = df(df[69]!='Bad')
-    
-    st.write(filtered[69].max())
+    st.write(df['WTG-A (2.82-FDR 2A) - POWER'].max())
 
