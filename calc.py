24c4cb115993731ff7e79686352bad2c764c6905
Aaron-Dhillon <110361766+Aaron-Dhillon@users.noreply.github.com>
2022-10-08 17:54:16 -0400

    Update

--- calc.py
+++ calc.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import streamlit as st
 
+data = st.container()
+
+max = st.container()
+
+
+
 df = pd.read_csv('WTG A.csv')
-st.write(df.columns)
+with data:
+    st.write(df.columns)
 
 
+with max:
+    
+    filtered = df(df[69]!='Bad')
+    
+    st.write(filtered[69].max())
 
