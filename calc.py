41c247fd41be640563c214fb2c77401455a48886
Aaron-Dhillon <110361766+Aaron-Dhillon@users.noreply.github.com>
2022-10-08 18:59:05 -0400

    Update

--- calc.py
+++ calc.py
@@ -1,18 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-
-data = st.container()
-
-max = st.container()
-
-
-
 df = pd.read_csv('WTG A.csv')
-df[df['WTG-A (2.82-FDR 2A) - POWER'].str.contains('Bad')==False]
 
-
-with data:
-    st.write(df.columns)
-    st.write(df['WTG-A (2.82-FDR 2A) - POWER'].max())
-
+faults= df['WTG-A (2.82-FDR 2A) - STATE AND FAULT'].contains('Fault').inde
