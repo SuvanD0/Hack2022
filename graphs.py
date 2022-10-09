a686d7911091d86f945ec2f3de6cba5b78228611
kathirm04 <109827714+kathirm04@users.noreply.github.com>
2022-10-09 05:30:01 -0400

    Add files via upload

--- graphs.py
+++ graphs.py
@@ -16,12 +16,17 @@ sampleDF = df.head(20)
 
 # fig1 = px.scatter(df,x= "turbine" ,y = 'blade1actual', opacity = .5, title = "Turbine 2 - BLADE 1 - SET VALUE")
 # st.write(fig1)
-xChoice = st.selectbox('X',df.columns)
-yChoice = st.selectbox('Y',df.columns)
+xChoice = st.selectbox('X-Axis',df.columns)
+yChoice = st.selectbox('Y-Axis',df.columns)
 fig1 = px.scatter(df,y= yChoice ,x = xChoice ,color='turbine', opacity = .5, title = "Turbine Data Values")
 fig1.update_layout(autotypenumbers='convert types')
-
 
 st.write(fig1)
 col1,col2,col3 = st.columns([1,4,1])
 col2.write("Press turbine icons on right side of graph to view specific turbines!")
+
+fig2 = px.line(df, x=xChoice, y = yChoice, color = 'turbine', title = "Power vs Turbine")
+fig2.update_layout(autotypenumbers='convert types')
+st.write(fig2)
+
+
